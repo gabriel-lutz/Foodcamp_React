eefc8892b1b4f9e82adcf665be071064b7bcc16b
thiagoribb <thiribeiro142@gmail.com>
2021-05-12 17:17:08 -0300

    Preparando review

--- src/components/Topo.js
+++
@@ -1,8 +0,0 @@
-export default function Topo(){
-    return(
-        <div className="topo">
-            <h1 className="nome-site">Foodcamp</h1>
-            <p>Sua comida em 6 minutos</p>
-        </div>
-    )
-}
